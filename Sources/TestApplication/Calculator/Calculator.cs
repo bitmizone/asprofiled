23ddfa8972e13ae9d2d47a140628ca189082f6fc
adamsonic <adamsonic@cf2cc628-70ab-11de-90d8-1fdda9445408>
2011-05-12 23:03:33 +0000

    documents updated

--- Sources/TestApplication/Calculator/Calculator.cs
+++ Sources/TestApplication/Calculator/Calculator.cs
@@ -2,14 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Diagnostics.Contracts;
+using LinFu.DesignByContract2.Contracts;
 
 namespace TestApplication.Calculator
 {
 
     class  Calculator : Object
     {
-        private int aa = 2;
         private Test test = new Test();
+
         [AsContract("c.test.member == 31 && divided > 1 || val == \"test\"", "divided / divisor > 0 && @returnValue == 0 || val == \"test\"",null)]
         public int Div(bool t, int divided, int divisor, string val, string delta, Calculator c)
         {
@@ -29,8 +31,47 @@ namespace TestApplication.Calculator
         }
     }
 
+    class MsContractsCalculator
+    {
+        [AsContract("arg2 != 0", null, null)]
+        public int Div(int arg1, int arg2)
+        {
+            Contract.Requires(arg2 != 0);
+            Contract.Ensures(Contract.Result<int>() >= 0 && Contract.OldValue(arg1) == 1);
+            return arg1 / arg2;
+        }
+
+        public void ObjectTest(Test t)
+        {
+            
+            Contract.Ensures(Contract.OldValue<Test>(t).member + 4 == t.member);
+            t.member += 4;
+        }
+
+        public int Factorial(int n)
+        {
+            Contract.Ensures(Contract.Result<int>() == n * Factorial(n - 1));
+            if (n == 0)
+                return 1;
+            return n * Factorial(n - 1);
+        }
+
+    }
+
+    class LinFuCalculator
+    {
+        
+        public int Mult(int arg1, int arg2)
+        {
+            
+            return 0;
+        }
+    }
+
+
     class Test
     {
+        
         public int member = 31;
     }
 
